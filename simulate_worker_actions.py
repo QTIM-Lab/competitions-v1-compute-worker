7ec673b4e3f3124e722da10174629238bf86c556
Ubuntu <azureuser@codalab-vm.1je0wiq2c5hurgblspm4cgzgge.bx.internal.cloudapp.net>
2021-03-30 20:49:47 +0000

    adding simulate_worker_actions.py

---
+++ simulate_worker_actions.py
@@ -0,0 +1,42 @@
+import os, sys, pdb
+from subprocess import Popen, call, check_output, CalledProcessError, PIPE
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SERVICE_PRINCIPAL_APPID=os.getenv("AZURE_CLIENT_ID")
+SERVICE_PRINCIPAL_CLIENT_SECRET=os.getenv("AZURE_CLIENT_SECRET")
+REGISTRY=os.getenv("AZURE_CONTAINER_REGISTRY")+".azurecr.io"
+# pdb.set_trace()
+
+
+# task_args should look like this:
+# task_args = {'file_name': 'mednist_docker_image.zip','user':'24'}
+# temp_dir = '/tmp/codalab'
+# unzip_dir = os.path.join(temp_dir,'tmp_unzip_dir')
+
+def login(registry=REGISTRY):
+    login_cmd = "docker login {registry} --username {appid} --password {secret}".format(
+        appid=SERVICE_PRINCIPAL_APPID, 
+        secret=SERVICE_PRINCIPAL_CLIENT_SECRET, 
+        registry=registry)
+    process = Popen(login_cmd.split(" "), stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    print("stdout: " + stdout)
+    print("stderr: " + stderr)
+
+
+login()
+
+
+def do_docker_pull(image_name):
+    print("Running docker pull for image: {}/{}".format(REGISTRY,image_name))
+    try:
+        cmd = ['docker', 'pull', "{}/{}".format(REGISTRY,image_name)]
+        docker_pull = check_output(cmd)
+        print("Docker pull complete for image: {0} with output of {1}".format(image_name, docker_pull))
+    except CalledProcessError as error:
+        print("Docker pull for image: {}/{} returned a non-zero exit code!".format(REGISTRY,image_name))
+
+do_docker_pull("user_1:docker_submission_v1")
+
