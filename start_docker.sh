46cb808e35b909ec4633a037e186742d05147eb0
Ubuntu <azureuser@medici-codalab-dev.raw0axe4pnpe3hvd40vlczdjja.bx.internal.cloudapp.net>
2021-06-18 05:10:07 +0000

    adding queue docker

---
+++ start_docker.sh
@@ -0,0 +1,10 @@
+docker run \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /tmp/codalab:/tmp/codalab \
+    -d \
+    --name compute_worker \
+    --env-file .env \
+    --restart unless-stopped \
+    --log-opt max-size=50m \
+    --log-opt max-file=3 \
+    codalab/competitions-v1-compute-worker:latest
